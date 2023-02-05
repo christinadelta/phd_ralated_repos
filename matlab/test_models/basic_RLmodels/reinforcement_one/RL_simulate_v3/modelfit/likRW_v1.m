e5d6b8df617838d32541a9fe965d287d0b9cf6a5
christinadelta <pljt159@live.rhul.ac.uk>
2023-02-05 19:59:22 +0000

    updated model fit for RW model

--- matlab/test_models/basic_RLmodels/reinforcement_one/RL_simulate_v3/modelfit/likRW_v1.m
+++ matlab/test_models/basic_RLmodels/reinforcement_one/RL_simulate_v3/modelfit/likRW_v1.m
@@ -3,10 +3,26 @@ function negll = likRW_v1(actions, rewards, alpha, beta)
 % compute negative log-likelihood
 
 % ----------------------
+% init q values
+Q = [0.5 0.5];
 
+trials = length(actions);
 
+for trl = 1:trials
 
+    % compute choice probabilities using the softmax function
+    p                   = exp(beta*Q) / sum(exp(beta*Q));
 
+    % compute choice probabilities for actual choices 
+    choiceProb(trl)     = p(actions(trl));
 
+    % update values
+    delta           = rewards(trl) - Q(actions(trl));  % pe
+    Q(actions(trl)) = Q(actions(trl)) + alpha * delta; % update Q values
+
+end % end of trials loop
+
+% compute negative log-likelihood
+negll = -sum(log(choiceProb));
 
 end % end of function
