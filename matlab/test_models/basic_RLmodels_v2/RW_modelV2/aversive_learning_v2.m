afcd74aaef5d37791dd6e097978dc4016f35ebb5
christinadelta <pljt159@live.rhul.ac.uk>
2023-02-21 16:43:03 +0000

    added RW both conditions plot

--- matlab/test_models/basic_RLmodels_v2/RW_modelV2/aversive_learning_v2.m
+++ matlab/test_models/basic_RLmodels_v2/RW_modelV2/aversive_learning_v2.m
@@ -31,5 +31,82 @@ addpath(fullfile(pwd,'functions'))
 outpath = fullfile(pwd, 'output'); addpath(outpath);
 figpath = fullfile(pwd, 'figures'); addpath(figpath);
 
+% initialise variables 
+subjects        = 1;
+params          = [.25 4];                % alpha and beta values 
+condition       = 2;                      % only stable condition for now (if 2 = stable and volatile)
+task            = 1;                      % stable without switch (if task = 2 then stable with one switch)
+
+if condition == 1
+    probs           = [.75 .25];          % probabilities of the stable condition
+else
+    probs           = [.75 .25; .80 .20]; % probabilities of the stable + volatile condition
+end
+
+labels          = {'alpha', 'beta'};      % for ploting
+condstring      = {'stable', 'volatile'}; % for ploting 
+trials          = 100;                    % per volatility condition
+alltrials       = trials*condition;       % total trials
+
+%% simulate dataset(s) for both conditions
+
+for sub = 1:subjects
+    
+    for cond = 1:condition
+        % simulate dataset
+        data{1,cond}                = aversivelearn_sim_v2(cond, probs, trials, outpath, task); % data is a structure containaining the simulation output
+        
+    end
+
+    allsub_data{1,sub}  = data; % if many subjects add them to cell
+
+    % for plotting we we need outcomes and underlying probabilities both conditions, so, concatinate the
+    % trials of the two conditions
+    feedback{sub}           = cat(1,data{1,1}.feedback, data{1,2}.feedback);
+    feedbackprob{sub}       = cat(1,data{1,1}.feedbackprob, data{1,2}.feedbackprob); % for ploting
+
+end
+    
+%% model the dataset(s)
+
+for sub = 1:subjects
+    
+    % extract this subject dataset
+    this_data               = allsub_data{1,sub};
+    for cond = 1:condition
+        [modelout{1,cond}]              = modelRW_v1(params, this_data{1,cond}, outpath);
+        
+    end
+    
+    % add all subjects model output to one cell
+    allsub_modelout{1,sub}  = modelout;
+
+    % for plotting we we need Qvalues, choice probabilities and choices both conditions, so, concatinate the
+    % trials of the two conditions
+    allQs{sub}      = cat(1, modelout{1,1}.Qvals, modelout{1,2}.Qvals);
+    allPs{sub}      = cat(1, modelout{1,1}.allPs(:,1), modelout{1,2}.allPs(:,1));
+    choices         = cat(2, modelout{1,1}.a, modelout{1,2}.a);
+    allcoices{sub}  = 2 - choices'; % convert to be 1 and 0;
+
+end
+
+%% plot model results for one sublect/dataset
+
+% prepare ploting inputs (for one dataset)
+probability = feedbackprob{1};
+actions     = allcoices{1};
+choiceProb  = allPs{1};
+Vvals       = allQs{1};
+
+% plot
+h = plotRW_V2(probability,choiceProb,Vvals,actions);
+
+% store the plot
+filename = fullfile(figpath, 'RW_singlesubPlot.fig');
+saveas(h, filename)
+
+
+
+
 
 
---
+++ matlab/test_models/basic_RLmodels_v2/functions/plotRW_V2.m
@@ -0,0 +1,107 @@
+function h = plotRW_V2(probability,choiceProb,Vvals,actions)
+% function h = plotRW_V2(probability,choiceProb,Vvals,actions)
+
+% plot RW results:
+% choices/actions
+% expected values(:,:)
+% choice probabilities (:,1)
+
+%% ----- 
+% define plotting parameters, plotting font-size, font types, etc..
+fnt         = 'Helvetica';
+fn          = 'Arial';
+abc         = 'ABC';
+fsl         = 14; 
+fsA         = 18;
+xsA         = -.15;
+ysA         = 1.1;
+yst         = 1.15; %0.15;
+fst         = 18;
+fsy         = 14;
+fsy2        = 10;
+% colours     = [1 .4 0;0 .4 1]; % orange and blue 
+% coloursEV   = [0.6350 0.0780 0.1840; 0 0.4470 0.7410];
+colours     = [0.6350 0.0780 0.1840;    % red
+    0 0.4470 0.7410;                    % blue 
+    0 0 0];                             % black
+lcol        = [.8 .8 .8];
+
+figure;
+set(gcf,'units','normalized');
+
+% define a few more ploting parameters
+nr          = 2; % number rows 
+nc          = 1; % number columns 
+sub_plts    = [1 2];
+ylm         = [-0.1 1.1];
+ylm2        = [.08 .16; .35 .5; -.2 1.2];
+j           = 1;
+
+dx          = [0; diff(probability)~=0];    % deal with feedback probabilities (when 1 = switch of probabilties occured)
+ix          = (find(dx))-1;                 % trials before each switch of probabilities
+tt          = 1:length(probability);        % total trials 
+
+%% make the subplots 
+
+% plot expected values as dotted lines 
+h(1)    = subplot(nr, nc, sub_plts(1)); 
+plot(Vvals(:,1),':','color',colours(1,:),'linewidth',2); hold on;   % plot Q value for vertical gabor in red
+plot(Vvals(:,2),':','color',colours(2,:),'linewidth',2); hold on;   % plot Q value for horizontal gabor in blue
+if ~any(isnan(ylm(1,:))), ylim(ylm(1,:)); end
+
+ym      = get(gca,'ylim');
+
+% specify trial ranges where volatility is high and plot vertical lines
+for i = 1:length(ix)
+    plot([tt(ix(i)); tt(ix(i))],ym','color',lcol,'linewidth',2);
+end
+
+% plot expected values lines 
+plot(Vvals(:,1),':','color',colours(1,:),'linewidth',2); hold on;
+plot(Vvals(:,2),':','color',colours(2,:),'linewidth',2); hold on;
+set(gca,'fontname',fn); 
+
+ylabel('Expected Values','fontsize',fsy);
+set(gca,'ticklength', [0 0]);
+
+% plot choice probabilities
+h(2) = subplot(nr,nc,sub_plts(2)); 
+plot(choiceProb(:,1),'color',colours(1,:),'linewidth',2); hold on;
+plot(probability,'color',colours(3,:),'linewidth',1); hold on;
+
+if all(~isnan(actions(:,1)))
+    plot(actions(:,1),'.','color','k');
+end
+
+set(gca,'fontname',fn);
+ylabel('Choice Probabilities','fontsize',fsy); hold on;
+set(gca,'ticklength', [0 0]);
+xlabel('Trial','fontsize',fsl);
+
+if ~any(isnan(ylm2(3,:))), ylim(ylm2(3,:)); end
+
+%% deal with legends
+
+% add plot number 
+for i=1:2
+    text(xsA,ysA,abc(i),'fontsize',fsA,'Unit','normalized','fontname',fn,'parent',h(i));
+%     set(h(i),'ylim',yl(i,:));
+end
+
+% add legend to plot A
+hlg = legend(h(1),{'EV vertical','EV horizontal'},'fontsize',fsl,'location','east');
+pos = get(hlg,'Position');
+pos(2) = 1.2*pos(2);
+set(hlg,'Position',pos);
+
+% add legend to plot B 
+hlg2 = legend(h(2),{'p(choose vertical)', 'p(reward|verical)'},'fontsize',fsl,'location','east');
+pos2 = get(hlg2,'Position');
+pos2(2) = 1.2*pos2(2);
+set(hlg2,'Position',pos2);
+
+% i =1;
+% text(0.5,yst,'Binary','fontsize',fst,'Unit','normalized','fontname',fnt,'parent',h(i),'HorizontalAlignment','Center','fontweight','bold');
+
+
+end % end of function 
